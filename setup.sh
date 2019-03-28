cf52d530acf5c44820409f7407cad45ed0408c3d
Marco Fugaro <marco.fugaro@gmail.com>
2019-03-28 19:10:13 +0100

    💀 remove dead formulas

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install cask node yarn git git-completition tldr
-brew cask install google-chrome slack hyper tableplus vscode sublime-text alfred spectacle aerial kap spotify vlc adobe-photoshop-cc adobe-illustrator-cc
+brew install cask node yarn git tldr
+brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc
 
 # Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
