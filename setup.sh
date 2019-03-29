30d3cf2dc09b098e624367618e7ab989cb637979
Marco Fugaro <marco.fugaro@gmail.com>
2019-03-29 11:15:28 +0100

    📖 add table tool

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install cask node yarn git tldr
-brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc
+brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc table-tool
 
 # Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
