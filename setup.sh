bb3a076b637d50d891b1799c484e79f3c183d549
Marco Fugaro <marcofugaro@Marcos-MacBook-Pro.local>
2019-03-29 11:28:09 +0100

    🐛 Fixes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install commands
 brew install cask node yarn git tldr
+
+# Install applications
+brew tap homebrew/cask-versions
 brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc table-tool
 
 # Show hidden files
@@ -11,5 +16,7 @@ defaults write com.apple.finder AppleShowAllFiles YES
 #  Allow Apps from Anywhere in macOS Sierra Security Gatekeeper
 sudo spctl --master-disable
 
-# remove password from sudo
-sudo echo "\n# remove password for sudo\n$(whoami) ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# Remove password from sudo
+sudo su
+echo "\n# remove password for sudo\n$(whoami) ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+exit
