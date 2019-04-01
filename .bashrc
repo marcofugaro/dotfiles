0dc714d97ea754c0cd1ef47ef2034b237bebb9bc
Marco Fugaro <marco.fugaro@gmail.com>
2019-04-01 15:22:37 +0200

    🖥  Add more commands

--- .bashrc
+++ .bashrc
@@ -7,14 +7,19 @@ alias chrome='open -a "Google Chrome"'
 # autocomplete ignores case
 bind 'set completion-ignore-case on'
 
+# bash & git completition
+# https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion || {
+  # if not found in /usr/local/etc, try the brew --prefix location
+  [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ] && \
+    . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+}
 
-# bash & git completition https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
 
-# start sexy-bash-prompt https://github.com/twolfson/sexy-bash-prompt
+# Start thefuck
+# https://github.com/nvbn/thefuck
+eval $(thefuck --alias)
+
+# start sexy-bash-prompt
+# https://github.com/twolfson/sexy-bash-prompt
 . ~/.bash_prompt
-
-# start rbenv
-eval "$(rbenv init -)"
--- setup.sh
+++ setup.sh
@@ -4,19 +4,30 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install commands
-brew install cask node yarn git tldr
+brew install cask node yarn git bash-completion tldr thefuck speedtest-cli diff-so-fancy httpie tree
+npm install --global fkill-cli
 
 # Install applications
 brew tap homebrew/cask-versions
-brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc table-tool
+brew cask install google-chrome google-chrome-canary firefox firefox-nightly slack hyper-canary tableplus visual-studio-code sublime-text alfred spectacle aerial kap spotify vlc table-tool google-drive-file-stream
+
+# Generate ssh key
+ssh-keygen -t rsa -b 4096 -C "marco.fugaro@gmail.com"
+
+# Copy ssh
+pbcopy < ~/.ssh/id_rsa.pub
+
+# Remove password from sudo
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+
+# Ignore case in tab completition
+echo "set completion-ignore-case on" | sudo tee -a /etc/inputrc
 
 # Show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
 
+# Show file extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 #  Allow Apps from Anywhere in macOS Sierra Security Gatekeeper
 sudo spctl --master-disable
-
-# Remove password from sudo
-sudo su
-echo "\n# remove password for sudo\n$(whoami) ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-exit
